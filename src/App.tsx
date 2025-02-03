4e39eab628f261038df74944eb1866a95020b86e
Lucas Deng <lucaszz@Lucass-MacBook-Pro.local>
2025-02-03 16:59:33 -0600

    Added a responsive styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
-
+import { Grid, GridItem, Stack } from "@chakra-ui/react";
 function App() {
-  return <Button colorPalette={"blue"}>Click Me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Navbar
+      </GridItem>
+      <Stack hideBelow="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Stack>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
