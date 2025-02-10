28d0ce04976efd57b93dd944f4b0e823d04e76a2
Lucas Deng <lucaszz@Lucass-MacBook-Pro.local>
2025-02-10 15:46:17 -0600

    Added a game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,14 +1,16 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card borderRadius={10} overflow={"hidden"}>
       <Image src={game.background_image} />
-      <CardBody></CardBody>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
     </Card>
   );
 };
