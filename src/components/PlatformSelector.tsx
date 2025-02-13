3d9256d7c4e1f67d389c629234ad6e859887877e
Lucas Deng <lucaszz@Lucass-MacBook-Pro.local>
2025-02-12 19:02:17 -0600

    Added platform drop down list

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,26 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import usePlatform from "../hooks/usePlatforms";
+import { BsChevronDown } from "react-icons/bs";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatform();
+
+  if (error) return null;
+
+  return (
+    <>
+      <Menu>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          Platforms
+        </MenuButton>
+        <MenuList>
+          {data.map((platform) => (
+            <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </>
+  );
+};
+
+export default PlatformSelector;
